1e5c33060a1648a68f3f950a5da3a2df7fec730b
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-01-31 15:34:25 -0500

    quick setup for running server etc...

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+session="priv_bird"
+tmux new-session -s ${session} -d
+tmux send -t ${session} "cd sample-site; npm start" ENTER
+tmux split-window -h
+tmux send -t ${session} "cd extension" ENTER
+tmux -2 attach-session -d
