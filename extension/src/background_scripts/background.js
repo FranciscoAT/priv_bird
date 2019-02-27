86c76238ada9c5ed9ac5a16e60c99dd3155e55e6
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-02-27 13:11:48 -0500

    get and send xml doc to background.js

--- extension/src/background_scripts/background.js
+++ extension/src/background_scripts/background.js
@@ -1,12 +1,22 @@
-// chrome.browserAction.setBadgeBackgroundColor({
-//     color: [255, 0, 0, 255]
-// });
-
+// Message Handler
 chrome.runtime.onMessage.addListener((msg, sender, res) => {
-    if (msg.action === 'alertNumber') {
-        console.log(msg);
-        chrome.browserAction.setBadgeText({
-            text: `${msg.value}`
-        });
+    if (msg.action === 'p3pData') {
+        res(false);
+        handleP3P(msg.value);
     }
 });
+
+function handleP3P(data) {
+    console.log(data);
+    // Handle p3p data here
+}
+
+// Commented out for future reference (setBadgeText is useful)
+// chrome.runtime.onMessage.addListener((msg, sender, res) => {
+//     if (msg.action === 'alertNumber') {
+//         console.log(msg);
+//         chrome.browserAction.setBadgeText({
+//             text: `${msg.value}`
+//         });
+//     }
+// });
--- extension/src/content_scripts/content.js
+++ extension/src/content_scripts/content.js
@@ -5,29 +5,58 @@ $(document).ready(() => {
      * the manifest json complains about localhost only urls
      */
 
-    var allowed_url = 'localhost:3000'
-    var current_url = $(location).attr("href");
+    let allowed_url = 'localhost:3000'
+    let current_url = $(location).attr("href");
     if (!current_url.includes(allowed_url)) {
-        console.log("Bad URL");
         return
-    } else {
-        console.log("Good URL");
     }
 
-    // All code should exist below this!!
+    // Get the p3p.xml and send it to background js
+    let p3pLocation = 'p3p.xml';
+    makeRequest('GET', `${current_url}${p3pLocation}`)
+        .then((data) => {
+            sendMessage('p3pData', data);
+        })
+        .catch((err) => {
+            console.log('Error occured in getting p3p xml file', err);
+        });
 
-    var num = Math.floor((Math.random() * 10) + 1);
-    console.log(num);
-    chrome.runtime.sendMessage({
-        action: "alertNumber",
-        value: num
-    }, (err) => {
-        if (err) {
-            console.log(error);
-        } else {
-            console.log("No error");
-        }
-    });
+    function sendMessage(msgName, msgContent) {
+        chrome.runtime.sendMessage({
+            action: msgName,
+            value: msgContent
+        }, (err) => {
+            if (err) {
+                console.log(`An error occured sending ${msgName} to background scripts.`, err);
+            }
+        });
+    }
 
-    
+
+    function makeRequest(method, url) {
+        return new Promise((res, rej) => {
+            var request = new XMLHttpRequest();
+            request.open(method, url);
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    var data = request.response;
+                    res(data);
+                } else {
+                    rej({
+                        status: request.status,
+                        statusText: request.statusText
+                    });
+                }
+            };
+
+            request.onerror = () => {
+                rej({
+                    status: request.status,
+                    statusText: request.statusText
+                });
+            };
+
+            request.send();
+        });
+    }
 });
