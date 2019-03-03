5e8ff17a23f257a5e3d16961b4284a56a8791f2a
DESKTOP-TQJF7SI\minou <minoucatou@gmail.com>
2019-03-03 12:52:40 -0500

    ajouter le code relié aux boutons dans fichier JS

--- extension/src/options.js
+++ extension/src/options.js
@@ -34,6 +34,43 @@ function saveUserPreferences() {
       document.getElementById("address").checked = checkBoxes.addr;
     });
   }
+  
+  
+  // --------------------------Ajouter du code pour changer l'icône--------------------------
+  //Modification to icon
+  //Sources: https://developer.chrome.com/extensions/browserAction
+  //https://stackoverflow.com/questions/35852715/changing-chrome-extension-icon
+  function iconColorChange(){
+	var status = "vert";
+	chrome.browserAction.setBadgeText( { text: status } );
+	chrome.browserAction.setBadgeBackgroundColor({color: [0,255,0,255]});
+  }
+  
+  
+  //remet l'icône comme avant
+  function iconColorDeChange(){
+	var status = "";
+	chrome.browserAction.setBadgeText( { text: status } );
+	chrome.browserAction.setBadgeBackgroundColor({color: [0,0,0]});
+  }
+ 
+ 
+  //NE fonctionne pas - code pour un pop up
+  //à moins que popup n'est pas un string mais un fichier html comme dans les exemples?
+  function iconPopUp2(){
+	var msg = "not work";	
+	chrome.browserAction.setBadgeText( { text: msg } );
+	chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+	
+	chrome.browserAction.setPopup({popup: "afficher msg"}); //fonctionne pas
+  }
+  
 
+  // ---------------------Action Listener pour les boutons du fichier HTML-------------------------
+  
   document.addEventListener("DOMContentLoaded", loadUserPreferences, false);
   document.getElementById("save").addEventListener("click", saveUserPreferences, false);
+  
+  document.getElementById("iconModification").addEventListener("click", iconColorChange, false);
+  document.getElementById("iconDeModification").addEventListener("click", iconColorDeChange, false);
+  document.getElementById("iconpopup").addEventListener("click", iconPopUp2, false);
