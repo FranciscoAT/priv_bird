4906a967d7ed543a9fd0a8831abdefacdb1dc7fc
Francisco Trindade <francisco.ad.trindade@gmail.com>
2019-03-07 16:05:45 -0500

    reworked setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 session="priv_bird"
 tmux new-session -s ${session} -d
-tmux send -t ${session} "cd sample-site; npm start" ENTER
+tmux send -t ${session} "cd sample-site; npm install; sleep 5s; npm run start" ENTER
 tmux split-window -h
-tmux send -t ${session} "cd extension; gulp watch" ENTER
+tmux send -t ${session} "cd extension; npm install; gulp watch" ENTER
 tmux split-window -v
-tmux send -t ${session} "cd remote-site; npm start-dev" ENTER
+tmux send -t ${session} "cd remote-site; npm install; npm run start-dev" ENTER
 tmux -2 attach-session -d
