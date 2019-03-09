13773a1b512a282863679342276cfd6e994054c6
NN <namchi.nguyen18@gmal.com>
2019-03-09 16:32:51 -0500

    pop-up for conflicts (wip)

---
+++ extension/src/popup.js
@@ -0,0 +1,8 @@
+
+//TODO - get message from background.js
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse) { //msg = response sent from bg.js, sender - tab id
+    var conflict = document.getElementById("conflict");
+    
+});
+
+
