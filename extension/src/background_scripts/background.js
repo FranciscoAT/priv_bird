4767222da67573cfb22ebafca73decb8792dccad
wes <minoucatou@gmail.com>
2019-03-17 11:16:59 -0400

    implementer fonction compare()

--- extension/src/background_scripts/background.js
+++ extension/src/background_scripts/background.js
@@ -44,16 +44,100 @@ function getLocalChromeValues(valuesToGetArr) {
 	});
 }
 
-function compare(values, p3p) {
+
+
+//data: objet JSON (tous les valeurs dans local storage)
+//p3p: converti fichier XML a un objet JSON
+function compare(data, p3p) {
 	// Reset the current conflicts
 	conflicts = {
 		"warnings": [],
 		"errors": []
 	};
 
-	console.log(values);
+
+	//P3P variables
+	var statement = p3p.POLICIES.POLICY.STATEMENT;
+
+	// more than one statement, the JSON object is an array
+	if (Array.isArray(statement)){
+
+		//go through each statement
+		for (var i = 0; i < statement.length; i++) {
+			console.log(i);
+			compareStatement(data, statement[i]);
+		}
+	} 
+
+	// one statement
+	else {
+		//P3P variables
+		var statement = p3p.POLICIES.POLICY.STATEMENT;
+		compareStatement(data, statement);
+
+
+		/*
+
+		var website_statement_data = p3p.POLICIES.POLICY.STATEMENT["DATA-GROUP"].DATA;
+		var website_retention = p3p.POLICIES.POLICY.STATEMENT.RETENTION; 
+
+		//valeurs calculées
+		var num_critical_data = totCriticalInfoStored(website_statement_data);
+		var num_general_data = website_statement_data.length - num_critical_data;
+		console.log("num_critical_data: " + num_critical_data)
+		console.log("num_general_data: " + num_general_data)
+		
+
+		//1 - user preference: share checkbox (info may be shared)
+		if(share_checkbox == true && website_statement_data.length>0){
+			conflicts.errors[conflicts["errors"].length] = "info will be shared";
+		}
+
+
+		//2 - user preference: retention-general
+		if (num_general_data>0){
+
+			// the website will store the user's information
+			if(general_retention == "no-retention" && !("no-retention" in website_retention)){
+				conflicts.errors[conflicts["errors"].length] = "general info will be stored";
+			}
+
+			//store info for more than a period of time
+			if(general_retention == "legal-retention" && ("legal-retention" in website_retention)){
+				conflicts.warnings[conflicts["warnings"].length] = "general info may be stored longer than legally required";
+			}
+
+			//store info for more than a period of time
+			if(general_retention == "legal-retention" && ("indefinitely" in website_retention)){
+				conflicts.errors[conflicts["errors"].length] = "general info will be stored for indeterminate period of time";
+				console.log("devrait ajouter message au array");
+			}
+		}
+
+		//3 - user preference: retention-critical
+		if (num_critical_data>0){
+
+			// the website will store the user's information
+			if(critical_rentention == "no-retention" && !("no-retention" in website_retention)){
+				conflicts.errors[conflicts["errors"].length] = "critical info will be stored";
+			}
+
+			//store info for more than a period of time
+			if(critical_rentention == "legal-retention" && ("legal-retention" in website_retention)){
+				conflicts.warnings[conflicts["warnings"].length] = "critical info may be stored longer than legally required";
+			}
+
+			//store info for more than a period of time
+			if(critical_rentention == "legal-retention" && ("indefinitely" in website_retention)){
+				conflicts.errors[conflicts["errors"].length] = "critical info will be stored for indeterminate period of time";
+			}
+		}
+		*/
+	}
+
 
 	/**
+	console.log(conflicts);
 	 * There are three values being passed in:
 	 * share-data-general, retention-general, retention-critical
 	 * Values for each:
@@ -67,6 +151,133 @@ function compare(values, p3p) {
 
 	updateBadge();
 }
+
+
+
+
+
+//compare one statement at time --------------------------------------------------------------
+function compareStatement(data, statement){
+	//user preferences variables
+	var share_checkbox = data["share-data-general"]; // if user allowed info to be shared
+	var general_retention = data["retention-general"]; // retention for general info
+	var critical_rentention = data["retention-critical"]; // rentention for critical info
+
+
+	//P3P variables
+	//var website_statement_data = p3p.POLICIES.POLICY.STATEMENT["DATA-GROUP"].DATA;
+	//var website_retention = p3p.POLICIES.POLICY.STATEMENT.RETENTION; 
+	var website_statement_data = statement["DATA-GROUP"].DATA;
+	var website_retention = statement.RETENTION; 
+
+	console.log("website_statement_data");
+	console.log(website_statement_data);
+	
+	//valeurs calculées
+	var num_critical_data = totCriticalInfoStored(website_statement_data);
+	var num_general_data = totGeneralInfoStored(website_statement_data); 
+
+	console.log("num_critical_data: " + num_critical_data)
+	console.log("num_general_data: " + num_general_data)
+	
+
+	//1 - user preference: share checkbox (info may be shared)
+	if(share_checkbox == true && website_statement_data.length>0){
+		conflicts.errors[conflicts["errors"].length] = "info will be shared";
+	}
+
+
+	//2 - user preference: retention-general
+	if (num_general_data>0){
+
+		// the website will store the user's information
+		if(general_retention == "no-retention" && !("no-retention" in website_retention)){
+			conflicts.errors[conflicts["errors"].length] = "general info will be stored";
+		}
+
+		//store info for more than a period of time
+		if(general_retention == "legal-retention" && ("legal-retention" in website_retention)){
+			conflicts.warnings[conflicts["warnings"].length] = "general info may be stored longer than legally required";
+		}
+
+		//store info for more than a period of time
+		if(general_retention == "legal-retention" && ("indefinitely" in website_retention)){
+			conflicts.errors[conflicts["errors"].length] = "general info will be stored for indeterminate period of time";
+		}
+	}
+
+	//3 - user preference: retention-critical
+	if (num_critical_data>0){
+
+		// the website will store the user's information
+		if(critical_rentention == "no-retention" && !("no-retention" in website_retention)){
+			conflicts.errors[conflicts["errors"].length] = "critical info will be stored";
+		}
+
+		//store info for more than a period of time
+		if(critical_rentention == "legal-retention" && ("legal-retention" in website_retention)){
+			conflicts.warnings[conflicts["warnings"].length] = "critical info may be stored longer than legally required";
+		}
+
+		//store info for more than a period of time
+		if(critical_rentention == "legal-retention" && ("indefinitely" in website_retention)){
+			conflicts.errors[conflicts["errors"].length] = "critical info will be stored for indeterminate period of time";
+		}
+	}
+}
+
+//---------------------------------------------------------------------------------------------
+
+
+function totCriticalInfoStored(website_statement_data){
+	
+	var counter = 0;
+
+	if (Array.isArray(website_statement_data)){
+		//loop through each attribute and check if the category
+		for (var i = 0; i < website_statement_data.length; i++) {
+			attribute_category = website_statement_data[i]["@attributes"].category;
+			console.log("loop ");
+		 	if(attribute_category == "financial"){
+		 		counter = counter + 1;
+		 	}
+		}
+
+	} else{
+		attribute_category = website_statement_data["@attributes"].category;
+		if(attribute_category == "financial"){
+		 	counter = counter + 1;
+		 }
+	}
+
+	 return counter;
+}
+
+
+function totGeneralInfoStored(website_statement_data){
+	
+	var counter = 0;
+
+	if (Array.isArray(website_statement_data)){
+		//loop through each attribute and check if the category
+		for (var i = 0; i < website_statement_data.length; i++) {
+			attribute_category = website_statement_data[i]["@attributes"].category;
+		 	if(attribute_category != "financial"){
+		 		counter = counter + 1;
+		 	}
+		}
+
+	} else{
+		attribute_category = website_statement_data["@attributes"].category;
+		if(attribute_category != "financial"){
+		 	counter = counter + 1;
+		 }
+	}
+
+	 return counter;
+}
+
+
 
 function updateBadge() {
 	let badgeColor = 'green';
@@ -164,7 +375,7 @@ function setBadgeTextColor(color, text) {
 // 	var website_store_data = p3p.POLICIES.POLICY.STATEMENT.RETENTION // JSON PURPOSE object
 
 // 	// website stores info for an underminate period of time
-// 	if ("indefinitly" in website_store_data){
+// 	if ("indefintely" in website_store_data){
 // 		conflict[conflict.length] = "this website must have the authorization to store your information for an underminate period of time";
 // 	}
 
